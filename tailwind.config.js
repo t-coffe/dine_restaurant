15fc7f9f37f741808ddeb1869ce19ef1a7674a10
t-coffe <tcoffe@edenschool.fr>
2023-12-19 14:58:06 +0100

    WIP : align items and center text

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./*.js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "beaver":"#9e7f66",
+        "codGray":"#111111",
+        "mirage":"#17192b",
+        "ebonyClay":"#242b37",
+        "shuttleGray":"#5c6779",
+      }
+    },
   },
   plugins: [],
 }
